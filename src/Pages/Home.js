2772afeac4d0ed055b3841abc49fd25d1eb70d7a
Manasi Pathak <manasi.pathak.01@gmail.com>
2023-11-20 01:55:46 +0530

    Login Page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "../Component/Header";
 
 
 const Home = () => {
   return (
-    <div>
-     
+    <div className='pt-16 bg-black min-h-[calc(100vh)]'>
+        <Header/>
     </div>
   );
 };
